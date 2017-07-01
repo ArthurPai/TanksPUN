677cca7b2be615b4c686de12257435e6dbf8e06a
ArthurPai <baidragoon@gmail.com>
2017-07-01 20:43:31 +0800

    connect to photon server

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,19 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace TanksPUN {
-    public class GameManager : Photon.PunBehaviour {
+namespace TanksPUN
+{
+    public class GameManager : Photon.PunBehaviour
+    {
         public static GameManager instance;
 
         void Awake()
         {
-            if(instance != null)
-            {
+            if (instance != null) {
                 DestroyImmediate(gameObject);
                 return;
             }
             DontDestroyOnLoad(gameObject);
             instance = this;
+        }
+
+        void Start()
+        {
+            PhotonNetwork.ConnectUsingSettings("TanksPUN_v1.0");
+        }
+
+        public override void OnConnectedToPhoton()
+        {
+            DebugLog("Connected to Photon");
+        }
+
+        public override void OnConnectedToMaster()
+        {
+            DebugLog("Connected to Master");
+        }
+
+        public override void OnFailedToConnectToPhoton(DisconnectCause cause)
+        {
+            DebugLog("Failed Connect");
+        }
+
+        public override void OnDisconnectedFromPhoton()
+        {
+            DebugLog("Disconnected");
+        }
+
+        void DebugLog(string msg)
+        {
+            Debug.Log("Photon: " + msg);
         }
     }
 }
