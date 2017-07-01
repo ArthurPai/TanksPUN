b351590a54590ebf57f83d5f4676ebb1bdc9e058
ArthurPai <baidragoon@gmail.com>
2017-07-01 20:31:22 +0800

    create Game Manager object

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TanksPUN {
+    public class GameManager : Photon.PunBehaviour {
+        public static GameManager instance;
+
+        void Awake()
+        {
+            if(instance != null)
+            {
+                DestroyImmediate(gameObject);
+                return;
+            }
+            DontDestroyOnLoad(gameObject);
+            instance = this;
+        }
+    }
+}
